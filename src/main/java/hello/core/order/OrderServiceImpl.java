9233d6c2c99461bbc6d2bcda4e54f9cd837d6b36
Kimdonggyu <qweszx33@naver.com>
2023-04-08 01:15:22 +0900

    feat:create DIP OCP problem create-time:2023-04-08 01:15

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,8 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+//import hello.core.discount.FixDiscountPolicy;
+//import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,13 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();// 이 코드 를 보면 DiscountPolicy 뿐만 아니라
+//    FixDiscountPolicy 구체 클래스 까지 의존하고있는것을 알수있다. 이렇게되면 DIP를 위반하게 되는것이다.
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();// FixDiscountPolicy 를 RateDiscountPolicy
+//    로 바꾸기위한 코드이지만 OrderServiceImpl에 코드 수정이 생김으로 이것은 OCP위반으로 이어지게된다.
+//    DIP(Dependency Inversion Principle), OCP(Open Close Principle)
+//    해결책은 아래와 같다.
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
